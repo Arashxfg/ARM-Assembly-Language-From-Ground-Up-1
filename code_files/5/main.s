e7e90dc0439c496386660ef6ef214afaf3409848
Arashxfg <arashxfg1379@gmail.com>
2025-02-09 10:30:17 +0330

    code_files_5

---
+++ code_files/5/main.s
@@ -0,0 +1,15 @@
+VAL1	RN		R1
+VAL2	RN		R2
+SUM		RN		R3
+;NUM	EQU		0X1234
+
+		AREA RN,CODE,READONLY
+		ENTRY
+		EXPORT __main
+__main
+		MOV		VAL1,#0X25
+		MOV		VAL2,#0X34
+		ADD		SUM,VAL1,VAL2
+
+stop	B		stop
+		END
