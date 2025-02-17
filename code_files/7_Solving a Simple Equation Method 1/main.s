ac44a66ed42683602e619b11551f00cb6526f25b
Arashxfg <arashxfg1379@gmail.com>
2025-02-17 22:27:39 +0330

    code_files_7

---
+++ code_files/7_Solving a Simple Equation Method 1/main.s
@@ -0,0 +1,20 @@
+;P = Q + R + S
+;LET Q = 2, R = 4, S = 5
+Q		EQU		2
+R		EQU		4
+S		EQU		5
+
+
+		AREA simpleEquation,CODE,READONLY
+		ENTRY
+		EXPORT __main
+__main
+		MOV		R1,#Q
+		MOV		R2,#R
+		MOV		R3,#S
+		
+		ADD		R0,R1,R2
+		ADD		R0,R0,R3
+		
+stop	B		stop
+		END
