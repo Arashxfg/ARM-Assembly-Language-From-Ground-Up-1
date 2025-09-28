8cb4d9d4f183027a1c92d54bb316cf6dade7ea85
Arashxfg <arashxfg1379@gmail.com>
2025-09-28 10:10:42 +0330

    code_files_16

---
+++ code_files/16_Adding Signed Data/main.s
@@ -0,0 +1,20 @@
+			AREA	MyCode,CODE,READONLY
+			ENTRY
+			EXPORT __main
+__main
+			LDR   	R0,=SIGN_DATA
+			MOV		R3,#9
+			MOV		R2,#0
+		
+L			LDRSB		R1,[R0]
+			ADD		R2,R2,R1
+			ADD		R0,R0,#1	;because we use byte(8bite) data we use #1, if we use 32bit data we should use #4 
+			SUBS	R3,R3,#1
+			BNE		L
+			
+stop		B		stop
+		
+SIGN_DATA	DCB		+13,-10,+9,+14,-18,-9,+12,-19,+16
+			
+			ALIGN
+			END
